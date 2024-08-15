3096a33c7b857f1ed66921c237f0ab25abadc5f0
EdwinNgui <edwin.nguim@gmail.com>
2024-08-14 22:21:32 -0400

    Fixed Port Issue

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-# backend/app.py
 from flask import Flask
 from flask_cors import CORS
 from modelPredictionService import model_api
@@ -8,6 +7,6 @@ app = Flask(__name__)
 CORS(app)  # Allow all origins by default
 app.register_blueprint(model_api, url_prefix='/api')
 
+# This block is for running the app locally for development purposes
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use the port from the environment variable
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
