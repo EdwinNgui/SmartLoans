ef5c9378eb077ad798662cbfac5b2d18dcd4c9bc
EdwinNgui <edwin.nguim@gmail.com>
2024-08-14 21:57:30 -0400

    Updated to Render (backend hosting service)

--- frontend/src/helpers/apiPredictionHandler.js
+++ frontend/src/helpers/apiPredictionHandler.js
@@ -1,6 +1,6 @@
 // Function to make API call to Flask backend
 export async function getPrediction(data) {
-  const response = await fetch('http://localhost:5000/api/predict', {
+  const response = await fetch('https://smartloans-w01a.onrender.com/api/predict', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
