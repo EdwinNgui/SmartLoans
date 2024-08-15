d01fd920a5d5f2c6021e831057b801118d8df535
EdwinNgui <edwin.nguim@gmail.com>
2024-08-14 22:36:10 -0400

    Favicon and Tab Name

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import Layout from '../components/Layout.jsx';
 import "../globals.css"
 
+import Head from "next/head"
+
 // Applies global application-level customizations
 function App({ Component, pageProps }) {
   return (
     <React.StrictMode>
+      <Head>
+        <link rel="icon" href="/FaviconLogo.ico" />
+        <title>SmartLoans | Loan Prediction Service</title>       
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
